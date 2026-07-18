2584ebe3816ce0864648e39c5e884a1dad7a7e2c
sreenyaaudireddy <sreenyaaudi@gmail.com>
2026-07-18 12:49:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 const sections = document.querySelectorAll("section");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEachEntry(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
 
+sections.forEach(section => {
+  section.classList.add("hidden");
+  observer.observe(section);
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.style.boxShadow = "0 5px 15px rgba(0,0,0,.15);
+  }
+  else {
+    header.style.boxShadow = "0 2px 10px rgba(0,0,0,.08);
+  }
+});
+
+const gallery = document.querySelectorAll(".gallery-box");
+gallery.forEach(box => {
+  box.addEventListener("click", () => {
+    alert("Gallery image coming soon.");
+  });
+});
+
+window.addEventListener("load", () => {
+  console.log("Sri Natyalaya Kuchipudi Dance Academy");
+});
